6c17607880240917b7f88fb0d848dc81d2473f7c
Jonathan Sage <sagejonathan.tesol@gmail.com>
2025-04-01 20:33:13 -0600

    new logo and title

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -26,7 +26,16 @@ const Navigation = () => {
   return (
     <nav className={`nav ${isScrolled ? "scrolled" : ""}`}>
       <Link to="/" className="nav-logo">
-        Mountain Stories
+        <svg
+          className="nav-logo-icon"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M4 28L12 8L20 28H4Z" fill="currentColor" />
+          <path d="M12 28L20 16L28 28H12Z" fill="white" />
+        </svg>
+        <span className="nav-logo-text">Mountain Stories</span>
       </Link>
       <button className="mobile-menu-button" onClick={toggleMobileMenu}>
         <i className={`fas ${isMobileMenuOpen ? "fa-times" : "fa-bars"}`}></i>
