ea5aabb1981da0a542f1e69311d4ed49c86c39da
Jonathan Sage <sagejonathan.tesol@gmail.com>
2025-04-04 17:30:48 -0600

    add all poems content & assets

--- src/pages/Poems/poems.ts
+++ src/pages/Poems/poems.ts
@@ -1,62 +1,555 @@
+//Images Imports
+import redRocks from "../../assets/poems/RedRocks.webp"
+import septAns from "../../assets/poems/SeptAns.webp"
+import chantIntime from "../../assets/poems/ChantIntime.webp"
+import mariposas from "../../assets/poems/Mariposas.webp"
+import stillness from "../../assets/poems/Stillness.webp"
+import naissance from "../../assets/poems/Naissance.webp"
+import highWinds from "../../assets/poems/HighWinds.webp"
+import glace from "../../assets/poems/Glace.webp"
+import kluane from "../../assets/poems/Kluane.webp"
+import lesHirondelles from "../../assets/poems/LesHirondelles.webp"
+import espejos from "../../assets/poems/Espejos.webp"
+import leCadreHumain from "../../assets/poems/LeCadreHumain.webp"
+import lAmeImmortelle from "../../assets/poems/LAmeImmortelle.webp"
+import melancholia from "../../assets/poems/Melancholia.webp"
+import mortality from "../../assets/poems/Mortality.webp"
+import losLobos from "../../assets/poems/LosLobos.webp"
+import theSnow from "../../assets/poems/TheSnow.webp"
+import lAutomne from "../../assets/poems/LAutomne.webp"
+import mtAlberta from "../../assets/poems/MtAlberta.webp"
+import colombia from "../../assets/poems/Colombia.webp"
+import forestSong from "../../assets/poems/ForestSong.webp"
+import liberté from "../../assets/poems/Liberté.webp"
+import laRivière from "../../assets/poems/LaRivière.webp"
+import leTableau from "../../assets/poems/LeTableau.webp"
 export const poems = [
   {
     id: 1,
-    title: "Summit Dawn",
-    content: `At first light, the Matterhorn stands
-Silent sentinel in dawn's first rays
-Its peak, a crown of golden fire
-Burns through morning's misty haze
-
-The air is crisp, the world still sleeps
-As shadows stretch across the snow
-Each step echoes in the stillness
-Where few have dared to go
-
-The mountain speaks in whispers
-Of ancient tales untold
-Of climbers past and present
-Braving the cold
-
-And as we reach the summit
-The world unfolds below
-A tapestry of peaks and valleys
-In morning's golden glow`,
-    image: "/vert.webp",
-    imageAnnotation: "Matterhorn at dawn, Zermatt, Switzerland",
+    title: "Red Rocks",
+    content: `Suspended, above the sweet-scented grass 
+ Of desert trails and the breath that keeps 
+ Echoing off the sandstone walls. 
+ With the wind, shapeshifting
+ Across the grains of time. 
+ 
+ Day bleeds into night
+ And the horizon beacons
+ - But here, there is no time.
+ Timeless. Through motion
+ We ascend beyond words and fears
+ -And here, we find ourselves. `,
+    image: redRocks,
+    imageAnnotation: "Climber negotiates a chimney on a rock route.",
     location: {
-      name: "Matterhorn, Zermatt, Switzerland",
+      name: "Red Rocks National Park, Nevada, USA.",
       latitude: 45.9766,
       longitude: 7.6586,
     },
   },
   {
     id: 2,
-    title: "Alpine Echoes",
-    content: `The Eiger's face looms dark and sheer
-A challenge to the bold and brave
-Its stories etched in every crevice
-In every shadow, every wave
+    title: "Sept Ans",
+    content: `Près de sept ans, à vaincre le meilleur de moi.
+ Cherchant dans la lumière du jour
+ Les secrets lunaires de la foudre.
 
-The wind howls through the crags
-Carrying whispers of the past
-Of triumphs and of tragedies
-Of moments meant to last
+ Ce numéro astral qui guette la sagesse
+ Apparaît maintint devant mon corps damné.
+ Toute ma raison se plie devant l'énigme du sphinx.
 
-The climber's path is never straight
-Through ice and rock and snow
-Each hold a testament to courage
-Each step a story to know
+ Mille parcours aie ‘je emprunté;
+ Errant les vérités innombrables et sacrés
+ Du cœur, du corps, de la vie et la mort.
 
-And in the silence of the heights
-Where few have dared to tread
-The mountain's voice still echoes
-Through the paths the brave have led`,
-    image: "/horz.webp",
-    imageAnnotation: "Eiger North Face, Grindelwald, Switzerland",
+ Mon être a erré dans les anciens décors
+ Que la terre et les astres ont sculpté.
+ Par grâce, mes yeux ont bu de ces cruches
+
+ Et mon corps c'est joint à la création
+ D'une danse macabre. Enfin, nous buvons nos larmes
+ À en finir, affirmant le soleil naissant.
+
+ Sur les flancs du Mont central
+ Les rayons du soleil courtois la rosée ;
+ Fessant d'un monde entier : un passé.
+
+ De la terre cassée et creusée aux feux,
+ Surgis une fleur bleuâtre fait de pierre
+ - D'où coule une éternelle fontaine.`,
+    image: septAns,
+    imageAnnotation: "Alpinist begins the traverse towards Taulliraju  East ridge.",
     location: {
-      name: "Eiger North Face, Grindelwald, Switzerland",
+      name: "Huascaran National Park, Ancash, Peru.",
       latitude: 46.5778,
       longitude: 8.0053,
     },
   },
+  {
+    id: 3,
+    title: "Chant Intime",
+    content: `Comme un chant intime, 
+ Fragile- et jeune encore, 
+ Je te reconnais, O cœur. 
+ 
+ Toi qui fredonnes et gémi
+ Parmi les villes et les monts enneigés.
+ 
+ Qui palpite sonore aux aurores. Silencieux,
+ Tu graves les cieux blanchis et les cimes inhumaines. 
+ 
+ Toi qui scintillant aux falaises où l’on s’éveille dans le rêve;
+ Rocher à la main, le rire qui éclate- le vide sous nos pieds. `,
+    image: chantIntime,
+    imageAnnotation: "Climber crosses a handrail on Pigeon Spire.",
+    location: {
+      name: "Bugaboo National Park, British Colombia, Canada.",
+      latitude: 46.5778,
+      longitude: 8.0053,
+    },
+  },
+  {
+    id: 4,
+    title: "Mariposas",
+    content: `Cuando los frutos de los árboles
+Me canta los cantos de las aves,
+Que pasan como aceite por las manos, 
+Como aire en las ramas,
+También lloro la piel de mis fantasmas;
+Tan dulce y despididas.`,
+    image: mariposas,
+    imageAnnotation: "Solo Ski-Mountaineer negotiates a gendarme in a no-fall zone. ",
+    location: {
+      name: "Mt. Hood, Hood National Forest, Oregon, USA.",
+      latitude: 46.5778,
+      longitude: 8.0053,
+    },
+  },
+  {
+    id: 5,
+    title: "Stillness",
+    content: `Hush now…your mind is loud;
+ Still among the valley and its eddies. 
+ Hush now… the wind is speaking;
+ Still with the snow forming. 
+ Do not think. The path is yet to be.`,
+    image: stillness,
+    imageAnnotation: " A group of skiers settles into basecamp on the Colombia Icefield.",
+    location: {
+      name: "Jasper National Park, Alberta, Canada.",
+      latitude: 46.5778,
+      longitude: 8.0053,
+    },
+  },
+  {
+    id: 6,
+    title: "Naissance",
+    content: `Aux premières lueurs du jour
+Les ombres se dévoilent autour de moi ;
+Et la lumière de ma frontale se joint
+À l’homogénéité de la clarté naissante.
+J’éteins et j’entends les mésanges chantées;
+Celles qui font de cette paroi une paroisse des anges. 
+
+Je continue mon parcours à travers le givre
+-Ces jeunes feuilles taillées par le ciel éclairé-
+Et je respire cette même aire qui soutient ma chair ;
+Délicat et enflammé, tels ces sculptures éphémères. 
+
+J’avance dans ce monde de rocs taillés par les fautes,
+Qui permet une progression assurée, fébrile et haute. 
+Sans elle, comment voir le monde ? 
+La paroi n’est jamais qu’une dalle uniforme, 
+Mais cache en elle des plaisirs fantômes. `,
+    image: naissance,
+    imageAnnotation: "Two climbers approach the base of Huarapasca.",
+    location: {
+      name: "Cordillera Negra, Peru.",
+      latitude: 46.5778,
+      longitude: 8.0053,
+    },
+  },
+  {
+    id: 7,
+    title: "High Winds",
+    content: `There is a silence in the high winds
+ Which deafens the voices within.
+ 
+ There is a voice in the high winds
+ Who judges our idols, which we bring. 
+ 
+There is a silence in the high winds
+Which calls us forwards- an act of courage foretold.  
+
+There is a voice in the high winds
+Who weighs our ideals, our deeds of merit.`,
+    image: highWinds,
+    imageAnnotation: "Looking towards the Earth, mid-climb, on Murchison Falls. ",
+    location: {
+      name: "Banff national park, Alberta, Canada.",
+      latitude: 46.5778,
+      longitude: 8.0053,
+    },
+  },
+  {
+    id: 8,
+    title: "Glace",
+    content: `À travers la rivière; 
+ Sur ce flanc de mont fier;
+ Une glace m’enivre :
+ Suspendue au loin
+ Parmi le calcaire fragile. 
+À la rencontre du temps 
+Et aux éléments; 
+Elle se dessine;
+Sans hâte, ni peur. 
+Comme une belle figure
+Ou les cils d’une fleur.`,
+    image: glace,
+    imageAnnotation: "Ice climber prepares to leave the belay on Curtain Call.",
+    location: {
+      name: "Jasper national park, Alberta, Canada.",
+      latitude: 46.5778,
+      longitude: 8.0053,
+    },
+  },
+  {
+    id: 9,
+    title: "Kluane",
+    content: `Reflected within the naked eye 
+Whose sight commands the sleigh:
+( Swaying across phantom winds)
+Are the mountains brought forth 
+By wild howls that reach the midnight sun.
+
+There, many rivers with maned rapids
+Gallop over the wild land, unhushed, 
+Streaming from the kiss of ancient glaciers and sun. 
+
+There, a forest dwells amid the scent of pine:
+Full-leafed and rich, like the gold pan’s hue;
+Nestled by the run of salmon wild.`,
+    image: kluane,
+    imageAnnotation: "Kaskawulsh Glacier terminal, viewed from Observation mountain.",
+    location: {
+      name: " Kluane national preserve, Yukon, Canada.",
+      latitude: 46.5778,
+      longitude: 8.0053,
+    },
+  }, 
+  {
+    id: 10,
+    title: "Les Hirondelles",
+    content: `N’entendez-vous pas le clocher des hirondelles? 
+ Ces alliées ailés annonçant le baptême continuel de l’être.
+ N’entendez-vous pas cette douce ardeur
+ Qui à l’oreille, nos viennent ces notes printanières? 
+Nous pour qui nos racines sont des fleurs. 
+ Des ruisseaux anciens pleins de futures.`,
+    image: lesHirondelles,
+    imageAnnotation: "Climber enters a loose section, high on the North Face of Mount Temple.",
+    location: {
+      name: "Banff national park, Alberta, Canada.",
+      latitude: 46.5778,
+      longitude: 8.0053,
+    },
+  }, 
+  {
+    id: 11,
+    title: "Espejos",
+    content: `En ojos profusos
+    Que nos son los míos, 
+     Ahí visto
+    Reflejado y fijo 
+    (como perlas de agua sobre las alas de las mariposas)
+    Tantas luces de mentiras 
+    Ascondiendo verdades sombrías
+    Un mundo hecho de dolor
+   Ahogado por mares de amor. 
+   Espejos de todos los colores,
+    Revelado por las mías. 
+ `,
+    image: espejos,
+    imageAnnotation: "Solo Climber negotiates an exposed ridge on Mt. Hungabee",
+    location: {
+      name: "Yoho national park, BC, Canada.",
+      latitude: 46.5778,
+      longitude: 8.0053,  
+    },
+  }, 
+  {
+    id: 12,
+    title: "Le Cadre Humain",
+    content: `La raison se cherche
+Parmi les couloirs cloîtrés,
+D’un temps d’où la mémoire
+C’est échappé d’elle-même.
+Alors, aux aurores, figea-t ’elle
+Son regard vif et morne,
+Et l’étincelle d’un soupire mondain
+Fendit le cadre humain.`,
+    image: leCadreHumain,
+    imageAnnotation: "Anonymous skier cresting the summit ridge towards Miner's Gulley.",
+    location: {
+      name: "Canmore, Alberta, Canada.",
+      latitude: 46.5778,
+      longitude: 8.0053,
+    },
+  }, 
+  {  
+    id: 13,
+    title: "L'Ame Immortelle",
+    content: `Ma communauté? 
+ Quelle flamme, quel vent,
+ Quelle être semblable,
+ Se reconnaît que dans la dissolution?`,
+    image: lAmeImmortelle,
+    imageAnnotation: "Bootpacking up Aemmer Couloir on Temple North Face. ",
+    location: {
+      name: "Banff national park, Alberta, Canada.",
+      latitude: 46.5778,
+      longitude: 8.0053,
+    },
+  }, 
+  {
+    id: 14,
+    title: "Melancholia",
+    content: `I would die in the auburn light
+ That peers down this canopy;
+ A darkening mist- pallor of the trees.
+Faded thus upon a bed of leaves
+ -Crimson as my lover’s lips-
+ Would I dream of the soft kiss of Spring.
+ 
+ And rise amid the thaw;
+ That rustles by, unawares, 
+ Across the fields in bloom. `,
+    image: melancholia,
+    imageAnnotation: "Winter scene on the Bow river.",
+    location: {
+      name: "Banff national park, Alberta, Canada.",
+      latitude: 46.5778,
+      longitude: 8.0053,
+    },
+  }, 
+  {
+    id: 15,
+    title: "Mortality",
+    content: `The world is on fire;
+ And love is the wind 
+ With which it spreads. 
+ 
+ Half-awoken breathes,
+ From our lips, float
+ In trances of mercy.
+ 
+ The ecstasy of lights
+ Dance upon our flesh
+ Like rosy-hewed sleep. `,
+    image: mortality,
+    imageAnnotation: "Mount Hungabee",
+    location: {
+      name: "Yoho national park, BC, Canada.",
+      latitude: 46.5778,
+      longitude: 8.0053,
+    },
+  },
+   {
+    id: 16,
+    title: "Los Lobos",
+    content: `By and by, the wolves in my chest
+Leap out into the night wind,
+And burrow their howls deep 
+Beneath the drifts of snow. 
+
+As I dream my visions of blinding light,
+The hordes of day abate my mind. 
+Still, I roam the depths and halls
+Of constant truths that remold the eye. 
+
+And wake, like a babe in the manger,
+Kept warm by the deadfall of noble grass. 
+Now leaping into cold- this season’s mirth.
+Erring the paths that the woodland guides me on,
+
+I hear the muffled songs of my heart. `,
+    image: losLobos,
+    imageAnnotation: "First light by Kindergarten Couloir. ",
+    location: {
+      name: "Kootenay national park, BC, Canada.",
+      latitude: 46.5778,
+      longitude: 8.0053,
+    },
+  }, 
+  {
+    id: 17,
+    title: "The Snow",
+    content: `The steep slopes have masked their edges,
+Now soft shimmered with the fallen snow. 
+Ice now is proud on the river's throne:
+Blue as the sky is wide. Winter lulls my eyes.
+ Soft burning, my naked cheeks creek with thee. 
+ I err the valleys and dust the groves at my feet.
+ No leaf is hidden to make bedding this eve. 
+The fir trees speak and share their scent to me;
+They offer their limbs for nesting against the snow. 
+Taking only what I need, I spread them in a palm.
+ I sleep in their hand, a warm creature rocked
+By wind and fall of breath. Graces rise
+Amid the branches dead; perfumed and green. 
+And I think to myself, of what mercy -
+Might I repay this conferring gift of peace?
+`,
+    image: theSnow,
+    imageAnnotation: "Winter scene in Kananaskis country. ",
+    location: {
+      name: "Kananaskis country, Alberta, Canada.",
+      latitude: 46.5778,
+      longitude: 8.0053,
+    },
+  },
+  {
+    id: 18,
+    title: "L'Automne",
+    content: `Au temps que la lumière
+Se fend au froid – et se raccourci
+Au compte de l’horloge comtoise,
+Le vent Automnale viens nous caresser;
+Et fait à notre âme se sort froissé
+Dont nous voyons dans ces champs éventré. `,
+    image: lAutomne,
+    imageAnnotation: "Unknown massif in the Coast Mountains.",
+    location: {
+      name: "British Columbia, Canada.",
+      latitude: 46.5778,
+      longitude: 8.0053,
+    },
+  },
+  {
+    id: 19,
+    title: " Mt. Alberta",
+    content: `This night, of stars set to slumber
+By the course of distant embers,
+Sets across the grey sky;
+Humming an ominous tune.
+Yonder the expanse of the valley,
+A cathedral of stone stands yet
+-Veiled by the burning Earth. 
+And we, pilgrims to Heaven's step,
+Despondent, lay by walls of clay.`,
+    image: mtAlberta,
+    imageAnnotation: "Mount Alberta veiled in smoke at twilight, viewed from the valley floor.",
+    location: {
+      name: "Jasper national park, Alberta, Canada",
+      latitude: 46.5778,
+      longitude: 8.0053,
+    },
+  }, 
+  {
+    id: 20,
+    title: "Colombia",
+    content: `Time, out of its snowy cloak
+Slithers into blossoming spring.
+Where dark skies now glisten white
+In the early morning light.
+
+Space, as if to yawn,
+Gaps the clouds fertile,
+Of a pale blue that seems to sing.`,
+    image: colombia,
+    imageAnnotation: "Mount Colombia, seen from Snowdome on the  Colombia icefield. ",
+    location: {
+      name: "Jasper national park, Alberta, Canada",
+      latitude: 46.5778,
+      longitude: 8.0053,
+    },
+  }, 
+  {
+    id: 6,
+    title: " Forest Song",
+    content: ` L’écorce de mon cœur,
+De sa douleur se dérobe;
+Aux murmures des pétales
+Qui s’éveille du sol blanchi. 
+
+Les mésanges chantent
+De leurs tiges égarées;
+Enveloppe l’air printanier
+De leurs songes distraits.
+
+Marchant les pierres du sentier,
+J’entends couler la sève des astres,
+Qui nous plonge de nouveau au mystère.`,
+    image: forestSong,
+    imageAnnotation: "Flood Falls.",
+    location: {
+      name: "Hope, British Columbia, Canada",
+      latitude: 46.5778,
+      longitude: 8.0053,
+    },
+  }, 
+  {
+    id: 6,
+    title: " Liberté",
+    content: `Air d’éveil;
+ Qui au matin résonne 
+ Sous mes pieds volants.
+Air d’éveil; 
+Capté sur ces blancs pétales,
+Du ciel tombé, comme un rêve.`,
+    image: liberté,
+    imageAnnotation: "Mt.Whymper in vermillion pass.",
+    location: {
+      name: "Kootenay national park, BC, Canada",
+      latitude: 46.5778,
+      longitude: 8.0053,
+    },
+  }, 
+  {
+    id: 6,
+    title: " La Rivière",
+    content: `Songe, mon cœur;
+Et quitte tes tréfonds intime.
+Envol toi au-delà de ma vue;
+Plane aux sons boisés qui nous entourent.
+Oublie la rigidité de mon corps;
+Échappes-toi telles ces glaces errant la rivière.
+Nous nous rencontrons de nouveau ailleurs.`,
+    image: laRivière,
+    imageAnnotation: "Bow river & Cascade Mountain.",
+    location: {
+      name: "Banff national park, Alberta, Canada",
+      latitude: 46.5778,
+      longitude: 8.0053,
+    },
+  }, 
+  {
+    id: 6,         
+    title: "Le Tableau",
+    content: `Ma blanche peau se fend,
+ Seul et sans témoin, aux sons
+ Et l’ardeur du vent chansonnant.
+ Celles qui bercent les monts enneigés;
+ Où vacillent les pierres, silencées par le givre de la nuit.
+ Ce tableau fécond sur laquelle la lumière expansive réjouit.`,
+    image: leTableau,
+    imageAnnotation: "Ball Range.",
+    location: {
+      name: "Kootenay national park, BC, Canada",
+      latitude: 46.5778,
+      longitude: 8.0053,
+    },
+  }, 
+  // {
+  //   id: 6,
+  //   title: "",
+  //   content: ``,
+  //   image: ,
+  //   imageAnnotation: "",
+  //   location: {
+  //     name: "",
+  //     latitude: 46.5778,
+  //     longitude: 8.0053,
+  //   },
+  // },
 ];
